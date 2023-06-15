e9254d409a1ef1f820cfa6ac21a8b95680c5a535
asim <asm.blkbs@gmail.com>
2023-06-15 20:43:57 +0300

    build deploy

---
+++ build/precache-manifest.5943773a33f5153b5123f221063c4abe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "181aa33a87256d9a1bbe",
+    "url": "/static/css/main.8807cca9.chunk.css"
+  },
+  {
+    "revision": "181aa33a87256d9a1bbe",
+    "url": "/static/js/main.181aa33a.chunk.js"
+  },
+  {
+    "revision": "ddde412f780efc496c7c",
+    "url": "/static/js/1.ddde412f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "83bad926d906468dda08d32c492badbb",
+    "url": "/index.html"
+  }
+];
